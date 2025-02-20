ad14d4f1943f6383f49984f8c060824a47b0af59
Balajicg5 <balaji989412@gmail.com>
2025-02-20 22:26:35 +0530

    fixed analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Analytics } from "@vercel/analytics/react"
 import { PDFDownloadLink, Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
 const departmentDatabase = {
     "CSE": {
