486dcdfd87d44805aebe7e245ac6b71e23832fa8
Balajicg5 <balaji989412@gmail.com>
2025-05-17 15:06:27 +0530

    removed api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'cgpa-calculator-backend-production.up.railway.app/api'
+const API_URL = 'cgpa-calculator-backend-production.up.railway.app'
 
 // Create axios instance
 const api = axios.create({
